3479e51d834fe7ea7fb1e5255c197b76c893d77c
ghadanfar2003 <ghadanfarheusen3@gmail.com>
2025-04-22 00:21:48 +0300

    first api save

---
+++ resources/views/creatProduct.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+
+</head>
+<body>
+
+<form action="/create" method="POST">
+@csrf
+<div class="container-fluid d-flex text-center align-items-md-center justify-content-center flex-column p-2">
+
+<input class="form-control w-75 p-2 m-1" type="text" name="name" placeholder="name">
+<input class="form-control w-75 p-2 m-1" type="number" step="0.1" name="price" placeholder="price">
+<button class="btn btn-primary type="submit"> save product </button>
+
+</div>
+</form>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+</body>
+
+
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
 
-Route::get('/', function () {
+
+Route::get('/home', function () {
     return view('welcome');
 });
+Route::get('/get',[ProductController::class, 'index']);
+Route::get('/create',[ProductController::class, 'create']);
+Route::post('/create',[ProductController::class, 'store']);
